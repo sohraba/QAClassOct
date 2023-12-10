0a5dfe4487bc04bf87b4edd0a9915a8364325aaa
sohrabahmad <sohrabahmad@sohrabs-air.lan>
2023-12-10 22:51:18 +0400

    Added Add.java file

---
+++ TestAutomation/src/test/java/com/test/testcase/Add.java
@@ -0,0 +1,12 @@
+package com.test.testcase;
+
+public class Add {
+	public static void main(String args[]) {
+		add(10, 20);
+		
+	}
+	public static void add(int a,int b) {
+		System.out.print(a+b);
+	}
+
+}
--- TestAutomation/src/test/java/com/test/testcase/Testing.java
+++ TestAutomation/src/test/java/com/test/testcase/Testing.java
@@ -1,5 +1,6 @@
 package com.test.testcase;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
